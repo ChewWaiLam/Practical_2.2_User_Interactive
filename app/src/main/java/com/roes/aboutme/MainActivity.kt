b887333942d6f38f502fd9dcb0660c2e8c339588
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2021-06-29 14:51:40 +0800

    Question 1: I think is TextView?
    Question 2:
    GONE
    
    Question 3:
    false
    
    Question 4:
    Button views are clickable, and on click, the attached click listener performs an action.

--- app/src/main/java/com/roes/aboutme/MainActivity.kt
+++ app/src/main/java/com/roes/aboutme/MainActivity.kt
@@ -2,10 +2,26 @@ package com.roes.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDone: Button = findViewById(R.id.button_Done)
+
+        btnDone.setOnClickListener(){
+            val txtResult: TextView = findViewById(R.id.textView_Result)
+            val txtName: TextView = findViewById(R.id.nickname_edit)
+
+            txtResult.text = txtName.text.toString()
+            txtResult.visibility = View.VISIBLE
+            btnDone.visibility = View.GONE
+            txtName.visibility = View.GONE
+
+        }
     }
 }
